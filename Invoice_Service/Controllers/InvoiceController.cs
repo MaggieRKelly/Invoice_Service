7bba1e6cf318f7ef3da7fc00dd8cff760eff12b1
Maggie Kelly <maggie.kelly8812@gmail.com>
2017-11-12 21:35:29 +0000

    Invoice Class changes

---
+++ Invoice_Service/Controllers/InvoiceController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Invoice_Service.Interfaces;
+using Invoice_Service.Models;
+//using Invoice_Service.Infrastructure;
+
+namespace Invoice_Service.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Invoice")]
+    public class InvoiceController : Controller
+    {
+        private readonly IInvoiceRepository _invoiceRepository;
+
+        public InvoiceController(IInvoiceRepository invoiceRepository)
+        {
+            _invoiceRepository = invoiceRepository;
+        }
+
+        [HttpGet]
+        public Task<IEnumerable<Invoice>> Get()
+        {
+            return GetInvoiceInternal();
+        }
+
+        private async Task<IEnumerable<Invoice>> GetInvoiceInternal()
+        {
+            return await _invoiceRepository.GetAllinvoices();
+        }
+
+        // GET api/invoices/5
+        [HttpGet("{id}")]
+        public Task<Invoice> Get(string id)
+        {
+            return GetInvoiceByIdInternal(id);
+        }
+
+        private async Task<Invoice> GetInvoiceByIdInternal(string id)
+        {
+            return await _invoiceRepository.GetInvoice(id) ?? new Invoice();
+        }
+
+        // POST api/invoices
+        [HttpPost]
+        public void Post([FromBody]string orderRef, string orderTotal, string customerId, string customerName, string customerAddress, string InvoiceTotal)
+        {
+            _invoiceRepository.AddInvoice(new Invoice() { OrderRef = orderRef, OrderTotal = orderTotal, CustomerId = customerId, CustomerName = customerName, CustomerAddress = customerAddress, InvoiceTotal = InvoiceTotal, CreatedOn = DateTime.Now, UpdatedOn = DateTime.Now, InvoicePending = true });
+        }
+
+        // PUT api/invoices/5
+        [HttpPut("{id}")]
+        public void Put(string id, [FromBody] bool InvoicePending)
+        {
+            _invoiceRepository.UpdateInvoice (id, InvoicePending);
+        }
+
+        // DELETE api/invoices/
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            _invoiceRepository.RemoveInvoice(id);
+        }
+
+
+    }
+}
